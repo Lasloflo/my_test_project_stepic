7ebf63838b661cd2cd184ed592355bf33e8d9810
Lasloflo <qrash1992@gmail.com>
2022-09-17 22:28:09 +0300

    update method to go to login page

--- test_main_page.py
+++ test_main_page.py
@@ -2,14 +2,27 @@ import pytest
 from selenium.webdriver.common.by import By
 import time
 from .pages.main_page import MainPage
-
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
-    page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
-    page.should_be_login_link()
+    # 1 способ перейти на другую страницу
+    # выполняем метод страницы - переходим на страницу логина (добавили return c объектом класса LoginPage и передали в переменную)
+    # login_page = page.go_to_login_page()  # добавили ретурн в метод go_to_login_page который теперь возвращает объект класса LoginPage c текущим браузером и текущей url на страницу с login
+    # login_page.should_be_login_page() # спользуем метод из класса LoginPage, для проверки страницы login_page
+    #page.should_be_login_link()  # проверяем есть ли кнопка на логин
+
+    # 2 способ перехода на страницу login_page
+    # убираем return в методе go_to_login_page в классе MainPage, реализуем этот метод с объектом page
+    page.go_to_login_page()
+    # инициалтзируем новый объект класса LoginPage
+    login_page = LoginPage(browser, browser.current_url)
+    # применяем метод из класса LoginPage для объекта LoginPage
+    login_page.should_be_login_page()
+
+
 
 # def test_guest_can_go_to_login_page(browser):
 #     link = "http://selenium1py.pythonanywhere.com/"
@@ -28,4 +41,4 @@ def test_guest_can_go_to_login_page(browser):
 #     browser.get(link)
 #     go_to_login_page(browser)
 
-# pytest - v --tb=line --language=en test_main_page.py
+# pytest -v --tb=line --language=en test_main_page.py
