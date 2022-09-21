7b27a1170e8bb3805a4b8787abdc06a096f773b3
Lasloflo <qrash1992@gmail.com>
2022-09-22 01:51:41 +0300

    finish commit

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -4,7 +4,7 @@ from .locators import BasketPageLocators
 
 class BasketPage(BasePage):
     def should_be_message_basket_is_empty(self):
-        assert self.is_element_present(*BasketPageLocators.BASKET_EMPTY_MESSAGE), "Not message about empty pasket"
+        assert self.is_element_present(*BasketPageLocators.BASKET_EMPTY_MESSAGE), "Not message about empty basket"
 
     def should_not_be_products_in_basket(self):
         assert self.is_not_element_present(*BasketPageLocators.PRODUCT_IN_BASKET), "There are products in basket"
--- test_main_page.py
+++ test_main_page.py
@@ -6,27 +6,24 @@ from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()  # открываем страницу
-    # 1 способ перейти на другую страницу
-    # выполняем метод страницы - переходим на страницу логина (добавили return c объектом класса LoginPage и передали в переменную)
-    # login_page = page.go_to_login_page()  # добавили ретурн в метод go_to_login_page который теперь возвращает объект класса LoginPage c текущим браузером и текущей url на страницу с login
-    # login_page.should_be_login_page() # спользуем метод из класса LoginPage, для проверки страницы login_page
-    # page.should_be_login_link()  # проверяем есть ли кнопка на логин
+link = "https://selenium1py.pythonanywhere.com/"
 
-    # 2 способ перехода на страницу login_page
-    # убираем return в методе go_to_login_page в классе MainPage, реализуем этот метод с объектом page
-    page.go_to_login_page()
-    # инициалтзируем новый объект класса LoginPage
-    login_page = LoginPage(browser, browser.current_url)
-    # применяем метод из класса LoginPage для объекта LoginPage
-    login_page.should_be_login_page()
 
-@pytest.mark.new_test
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self, browser):
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
+
+    def test_guest_should_see_login_link(self, browser):
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
+
+
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-    link = "https://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.go_to_basket_page()
@@ -34,21 +31,6 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     basket_page.should_not_be_products_in_basket()
     basket_page.should_be_message_basket_is_empty()
 
-# def test_guest_can_go_to_login_page(browser):
-#     link = "http://selenium1py.pythonanywhere.com/"
-#     browser.get(link)
-#     time.sleep(15)
-#     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-#     login_link.click()
 
-#
-#
-# def go_to_login_page(browser):
-#     login_link = browser.find_element(By.CSS_SELECTOR,"#login_link")
-#     login_link.click()
-#
-# def test_guest_can_go_to_login_page(browser):
-#     browser.get(link)
-#     go_to_login_page(browser)
 
 # pytest -v --tb=line --language=en test_main_page.py
